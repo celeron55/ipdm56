61f04aea0b2461368a38ad181e943722d4220deb
Perttu Ahola <celeron55@gmail.com>
2025-05-10 11:11:40 +0300

    ipdmrust: Adjust can_simulator.rs to match the car

--- ipdmrust/app_example/src/can_simulator.rs
+++ ipdmrust/app_example/src/can_simulator.rs
@@ -20,172 +20,120 @@ impl CanSimulator {
     pub fn update(&mut self, millis: u64) {
         // You can generate these using util/generate_can_simulator_txframe.py
 
-        if self.i % 27 == 0 {
+		// Inverter
+        if self.i % 23 == 0 {
             self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x50).unwrap(),
-                bxcan::Data::new(b"\x17\x20\x00\x00\x00\x00\x96\x00").unwrap(),
+                bxcan::StandardId::new(0x1DA).unwrap(),
+                bxcan::Data::new(b"\x95\x32\x18\x00\x00\x01\x02\x45").unwrap(),
             ));
         }
-        if self.i % 27 == 1 {
+        if self.i % 23 == 1 {
             self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x51).unwrap(),
-                bxcan::Data::new(b"\x00\x00\xff\xff\x00\x00\x00\x00").unwrap(),
+                bxcan::StandardId::new(0x55A).unwrap(),
+                bxcan::Data::new(b"\x1a\x36\x37\x00\x5f\x00\x5b\x28").unwrap(),
             ));
         }
-        if self.i % 27 == 2 {
+		// Inverter control
+        if self.i % 23 == 2 {
             self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x52).unwrap(),
-                bxcan::Data::new(b"\x15\x1d\x1a\x12\x00\x00\x00\x00").unwrap(),
+                bxcan::StandardId::new(0x300).unwrap(),
+                bxcan::Data::new(b"\x01\x0b\xa9\x0c\x0c\x00\x00\x00").unwrap(),
             ));
         }
-        if self.i % 27 == 3 {
-            // ipdm1 (includes pm state and pm contactor reason)
+        if self.i % 23 == 3 {
             self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x550).unwrap(),
-                bxcan::Data::new(b"\x41\x00\x00\x32\xd0\x15\x00\xe6").unwrap(),
+                bxcan::StandardId::new(0x301).unwrap(),
+                bxcan::Data::new(b"\x00\x00\x71\x00\x00\x00\x00\x00").unwrap(),
             ));
         }
-        if self.i % 27 == 4 {
-            // BMS (includes main contactor status)
+        if self.i % 23 == 4 {
             self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x30).unwrap(),
-                bxcan::Data::new(b"\x07\x0b\xd1\x00\x00\x00\x00\x00").unwrap(),
+                bxcan::StandardId::new(0x1D4).unwrap(),
+                bxcan::Data::new(b"\x6e\x6e\x00\x00\x07\x44\x01\x28").unwrap(),
             ));
         }
-        if self.i % 27 == 5 {
-            // BMS (includes cell voltages and temperatures)
+        if self.i % 23 == 5 {
             self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x31).unwrap(),
-                bxcan::Data::new(b"\x18\x21\x85\x06\x0a\x00\x00\x09").unwrap(),
+                bxcan::StandardId::new(0x50B).unwrap(),
+                bxcan::Data::new(b"\x00\x00\x06\xc0\x00\x00\x00").unwrap(),
             ));
         }
-        if self.i % 27 == 6 {
-            // BMS (includes SoC)
+        if self.i % 23 == 6 {
             self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x32).unwrap(),
-                bxcan::Data::new(b"\x00\xa3\x00\x3c\x0d\xac\xb1\x00").unwrap(),
+                bxcan::StandardId::new(0x11A).unwrap(),
+                bxcan::Data::new(b"\x4e\x40\x00\xaa\xc0\x00\x00\x6b").unwrap(),
             ));
         }
-        if self.i % 27 == 7 {
+		// BMS
+        if self.i % 23 == 7 {
             self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x33).unwrap(),
-                bxcan::Data::new(b"\x00\x00\x00\x00\x03\x01\x05\x3e").unwrap(),
+                bxcan::StandardId::new(0x100).unwrap(),
+                bxcan::Data::new(b"\x07\x0b\xa5\x00\x00\x95\x00\x00").unwrap(),
             ));
         }
-        if self.i % 27 == 8 {
+        if self.i % 23 == 8 {
             self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x34).unwrap(),
-                bxcan::Data::new(b"\x10\x18\x00\x00\x00\x00\x00\x00").unwrap(),
+                bxcan::StandardId::new(0x101).unwrap(),
+                bxcan::Data::new(b"\x19\xe1\x9f\x07\x0b\x00\x0b\x0b").unwrap(),
             ));
         }
-        if self.i % 27 == 9 {
+        if self.i % 23 == 9 {
+            self.txbuf.push(bxcan::Frame::new_data(
+                bxcan::StandardId::new(0x102).unwrap(),
+                bxcan::Data::new(b"\x0b\xcc\x02\x58\x0b\xb8\xe4\x00").unwrap(),
+            ));
+        }
+        if self.i % 23 == 10 {
+            self.txbuf.push(bxcan::Frame::new_data(
+                bxcan::StandardId::new(0x104).unwrap(),
+                bxcan::Data::new(b"\x10\x04\x00\x00\x00\x00\x00\x00").unwrap(),
+            ));
+        }
+		// CCS controller
+        if self.i % 23 == 16 {
+            self.txbuf.push(bxcan::Frame::new_data(
+                bxcan::StandardId::new(0x506).unwrap(),
+                bxcan::Data::new(b"\x01\x00\x00\x00\x00\x19\x00\x00").unwrap(),
+            ));
+        }
+		// Outlander PHEV heater
+        if self.i % 23 == 17 {
+            self.txbuf.push(bxcan::Frame::new_data(
+                bxcan::StandardId::new(0x398).unwrap(),
+                bxcan::Data::new(b"\x01\x1a\x11\x5a\x5b\x28\x00\x00").unwrap(),
+            ));
+        }
+		// Outlander PHEV heater request
+        if self.i % 23 == 18 {
+            self.txbuf.push(bxcan::Frame::new_data(
+                bxcan::StandardId::new(0x188).unwrap(),
+                bxcan::Data::new(b"\x03\x50\x32\x4d\x00\x00\x00\x00").unwrap(),
+            ));
+        }
+        if self.i % 23 == 19 {
+            self.txbuf.push(bxcan::Frame::new_data(
+                bxcan::StandardId::new(0x285).unwrap(),
+                bxcan::Data::new(b"\x00\x00\x14\x21\x90\xfe\x0c\x10").unwrap(),
+            ));
+        }
+        // Outlander OBC (copied from EV-Omega)
+        if self.i % 23 == 20 {
             self.txbuf.push(bxcan::Frame::new_data(
                 bxcan::StandardId::new(0x377).unwrap(),
                 bxcan::Data::new(b"\x05\x9c\x01\x12\x45\x4b\x40\x22").unwrap(),
             ));
         }
-        if self.i % 27 == 10 {
+        if self.i % 23 == 21 {
             // BMS (includes main contactor status)
             self.txbuf.push(bxcan::Frame::new_data(
                 bxcan::StandardId::new(0x389).unwrap(),
                 bxcan::Data::new(b"\x97\x00\x01\x3f\x40\xd0\x00\x00").unwrap(),
             ));
         }
-        if self.i % 27 == 11 {
+        if self.i % 23 == 22 {
             self.txbuf.push(bxcan::Frame::new_data(
                 bxcan::StandardId::new(0x38A).unwrap(),
                 bxcan::Data::new(b"\x44\x3f\x81\x1b\x04\x00\x00\x00").unwrap(),
-            ));
-        }
-        if self.i % 27 == 12 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x569).unwrap(),
-                bxcan::Data::new(b"\xaa\xa8\xa2\x2a\x00\x00\x00\x00").unwrap(),
-            ));
-        }
-        if self.i % 27 == 13 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x568).unwrap(),
-                bxcan::Data::new(b"\x8a\x3a\xaa\x8a\xaa\x8a\xa8\xaa").unwrap(),
-            ));
-        }
-        if self.i % 27 == 14 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x563).unwrap(),
-                bxcan::Data::new(b"\xaa\xaa\xaa\xaa\x82\x2a\x00\x00").unwrap(),
-            ));
-        }
-        if self.i % 27 == 15 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x664).unwrap(),
-                bxcan::Data::new(b"\x02\x83\x06\x32\xf3\x00\x00\x00").unwrap(),
-            ));
-        }
-        if self.i % 27 == 16 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x663).unwrap(),
-                bxcan::Data::new(b"\x01\x31\x30\x31\x00\x00\x00\x00").unwrap(),
-            ));
-        }
-        if self.i % 27 == 17 {
-            // Outlander heater
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x398).unwrap(),
-                bxcan::Data::new(b"\x01\x1c\x13\x5e\x5f\x28\x00\x00").unwrap(),
-            ));
-        }
-        if self.i % 27 == 18 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x630).unwrap(),
-                bxcan::Data::new(b"\x00\x00\x00\x00\x00\x00\x00\x00").unwrap(),
-            ));
-        }
-        if self.i % 27 == 19 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x62D).unwrap(),
-                bxcan::Data::new(b"\x00\x00\x00\x00\x00\x00\x00\x00").unwrap(),
-            ));
-        }
-        if self.i % 27 == 20 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x6BD).unwrap(),
-                bxcan::Data::new(b"\x00\x00\x00\x00\x00\x00\x00\x00").unwrap(),
-            ));
-        }
-        if self.i % 27 == 21 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x500).unwrap(),
-                bxcan::Data::new(b"\x00\x08\x00\x00\x00\x00\x00\x1e").unwrap(),
-            ));
-        }
-        if self.i % 27 == 22 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x506).unwrap(),
-                bxcan::Data::new(b"\x00\x00\x00\x00\x00\x00\x00\x00").unwrap(),
-            ));
-        }
-        if self.i % 27 == 23 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x521).unwrap(),
-                bxcan::Data::new(b"\x00\x09\xff\xff\xd9\x4f").unwrap(),
-            ));
-        }
-        if self.i % 27 == 24 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x525).unwrap(),
-                bxcan::Data::new(b"\x04\x0b\x00\x00\x00\x71").unwrap(),
-            ));
-        }
-        if self.i % 27 == 25 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x527).unwrap(),
-                bxcan::Data::new(b"\x06\x08\x00\x00\x8f\xca").unwrap(),
-            ));
-        }
-        if self.i % 27 == 26 {
-            self.txbuf.push(bxcan::Frame::new_data(
-                bxcan::StandardId::new(0x50).unwrap(),
-                bxcan::Data::new(b"\x17\x20\x00\x00\x00\x00\x96\x00").unwrap(),
             ));
         }
 
